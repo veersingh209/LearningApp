f2451e14253eea9d1a3930388ff1ee79d912e8f1
Veer Singh <veersingh209@icloud.com>
2022-08-25 16:08:16 -0700

    Data parsing

---
+++ LearningApp/Models/Models.swift
@@ -0,0 +1,56 @@
+//
+//  Models.swift
+//  LearningApp
+//
+//  Created by Veer Singh on 8/25/22.
+//
+
+import Foundation
+
+struct Module: Decodable, Identifiable {
+    
+    var id: Int
+    var category: String
+    var content: Content
+    var test: Test
+    
+}
+
+struct Content: Decodable, Identifiable {
+    
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var lessons: Lessons
+    
+}
+
+struct Lessons: Decodable, Identifiable {
+    
+    var id: Int
+    var title: String
+    var video: String
+    var duration: String
+    var explantation: String
+    
+}
+
+struct Test: Decodable, Identifiable {
+    
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var questions: [Questions]
+    
+}
+
+struct Questions: Decodable, Identifiable {
+    
+    var id: Int
+    var content: String
+    var correctIndex: Int
+    var answers: [String]
+    
+}
